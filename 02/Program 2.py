06eb99227272ecf9bca8234e1eed948cbe157b51
ZHANG CHUCHU <zhangchuchu@zhangchuchu.local>
2019-09-20 21:06:08 +0800

    Homework 2

---
+++ 02/Program 2.py
@@ -0,0 +1,9 @@
+CelsiusTemp = input("Input a number for the temperature in Celsius: ")
+while not CelsiusTemp:
+    CelsiusTemp = input("Input a number for the temperature in Celsius: ")
+
+try:
+    FahrenheitTemp = float(CelsiusTemp) * 1.8 + 32
+    print("When the temperature in Celsius is {0}, temperature in Fahrenheit is {1:5.3f}".format(CelsiusTemp, FahrenheitTemp))
+except ValueError:
+    print("Error, your input for the temperature is not a number. ")
