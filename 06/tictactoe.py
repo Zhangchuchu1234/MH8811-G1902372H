8dfdbe35881885cc871d2586e5df1b7e9caf7e77
Zhang Chuchu <zhangchuchu568@gmail.com>
2019-10-23 23:08:25 +0800

    Homework 6

--- 06/tictactoe.py
+++ 06/tictactoe.py
@@ -5,13 +5,15 @@ def TicTacDraw(board):
         n = len(board[i])
         for j in range(n):
             if board[i][j] == 0:
-                o = o + " O |"
+                o = o + " O "
             elif board[i][j] == 1:
-                o = o + " X |"
+                o = o + " X "
             elif board[i][j] == 2:
-                o = o + "   |"
-        print(o[:-1])
+                o = o + "   "
 
-        if i is not number_row-1:
+            if j is not (n-1):
+                o = o+"|"
+        print(o)
+
+        if i is not (number_row-1):
             print("----"*(n-1) + "---")
-
