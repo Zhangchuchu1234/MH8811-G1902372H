d4ac4c7aaf8441aa6b17ae176bcb2ffcbc2b0da7
Zhang Chuchu <zhangchuchu568@gmail.com>
2019-10-23 22:42:19 +0800

    Homework 6

--- 06/H1.py
+++ 06/H1.py
@@ -6,6 +6,10 @@ except:
     print("Input error!")
     exit()
 
+if password_length < 4:
+    print("Input length should be larger or equal to 4! ")
+    exit()
+
 password = genPassword(password_length)
 
 print("A random password of length {0} is {1}".format(password_length, password))
